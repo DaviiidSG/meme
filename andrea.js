0bc0999bbacd339e38c4dad36c93623ce6c53fd1
David Serrano <110501225+DaviiidSG@users.noreply.github.com>
2023-01-10 19:45:01 -0400

    First commit

---
+++ andrea.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const btnSi = document.getElementById('si');
+const btnNo = document.getElementById('no');
+const container = document.getElementById('container');
+const meme = document.getElementById('meme');
+
+btnSi.onclick = () => {
+  const cancionHot = new Audio('./src/audio-modo-hot.mp3');
+
+  cancionHot.play();
+  meme.style.height = '100%';
+  container.style.display = 'none';
+};
+
+btnNo.onmouseenter = (e) => { moverRandom(e.target) };
+
+function moverRandom(elm) {
+  elm.style.position = 'absolute';
+  elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+  elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
